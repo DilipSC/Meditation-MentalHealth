0913d1fd27f10175af4b7d101959ec2ebb9e15a7
DilipSC <dilipchakravarthi570@gmail.com>
2024-10-19 01:08:42 +0530

    Java-FX Integration

--- Mental_health/src/App.java
+++ Mental_health/src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        new LoginPage();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file (make sure the path is correct)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LP.fxml"));
+            Parent root = loader.load();
+            
+            // Set the scene
+            Scene scene = new Scene(root, 300, 200); // Adjust width and height as needed
+            primaryStage.setTitle("Mindful Oasis");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ Mental_health/src/LP.java
@@ -0,0 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+
+public class LP{
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+        System.out.println("ButtonLogin triggered");
+
+    }
+
+    @FXML
+    void btnSignUp(ActionEvent event) {
+        System.out.println("ButtonSIgnup triggered");
+
+    }
+
+}
